4343593823d690bf385cb7927a04af4a5a453d37
anzharnugraha26 <anzharnugraha26@gmail.com>
2023-03-30 14:23:00 +0700

    t

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -431,53 +431,45 @@
 
         <section class="projects section-padding" id="section_4">
             <div class="container">
-                <div class="row" id="table_data">
+                <div class="col-lg-8 col-md-8 col-12 ms-auto" data-aos="zoom-in">
+                    <div class="section-title-wrap d-flex justify-content-center align-items-center mb-4">
+                        <img src="{{ asset('element/tmp/images/white-desk-work-study-aesthetics.jpg') }}"
+                            class="avatar-image img-fluid" alt="">
 
-                    <div class="col-lg-8 col-md-8 col-12 ms-auto" data-aos="zoom-in">
-                        <div class="section-title-wrap d-flex justify-content-center align-items-center mb-4">
-                            <img src="{{ asset('element/tmp/images/white-desk-work-study-aesthetics.jpg') }}"
-                                class="avatar-image img-fluid" alt="">
-
-                            <h2 class="text-white ms-4 mb-0">Projects</h2>
-                        </div>
+                        <h2 class="text-white ms-4 mb-0">Projects</h2>
                     </div>
+                </div>
+                <div id="table_data">
+                    <div class="row">
+                        <div class="clearfix"></div>
 
-                    <div class="clearfix"></div>
+                        <?php $pr = DB::table('project')->paginate(3); ?>
 
-                    <?php $pr = DB::table('project')->paginate(3); ?>
-                   
-                    @foreach ($pr as $i)
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <div class="projects-thumb">
-                                <div class="projects-info">
-                                    <small class="projects-tag text-white">{{ $i->jenis }}</small>
+                        @foreach ($pr as $i)
+                            <div class="col-lg-4 col-md-6 col-12">
+                                <div class="projects-thumb">
+                                    <div class="projects-info">
+                                        <small class="projects-tag text-white">{{ $i->jenis }}</small>
 
-                                    <h3 class="projects-title">{{ $i->nama }}</h3>
+                                        <h3 class="projects-title">{{ $i->nama }}</h3>
+                                    </div>
+
+                                    <a href="{{ $i->url }}" target="_blank" class="">
+                                        <img src="{{ asset('element/tmp/images/' . $i->image) }}"
+                                            class="projects-image img-fluid" alt="">
+                                    </a>
                                 </div>
+                            </div>
+                        @endforeach
 
-                                <a href="{{ $i->url }}" target="_blank" class="">
-                                    <img src="{{ asset('element/tmp/images/' . $i->image) }}"
-                                        class="projects-image img-fluid" alt="">
-                                </a>
+                        <div class="row">
+                            <div class="col-md-6">
+                                {!! $pr->links() !!}
                             </div>
                         </div>
-                    @endforeach
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            {!! $pr->links() !!}
-                        </div>
                     </div>
-
-
-
-
-
-                    
-
-
-
                 </div>
+
             </div>
         </section>
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,18 +1,5 @@
-<div class="row" id="table_data">
+<div class="row">
 
-    <div class="col-lg-8 col-md-8 col-12 ms-auto" data-aos="zoom-in">
-        <div class="section-title-wrap d-flex justify-content-center align-items-center mb-4">
-            <img src="{{ asset('element/tmp/images/white-desk-work-study-aesthetics.jpg') }}"
-                class="avatar-image img-fluid" alt="">
-
-            <h2 class="text-white ms-4 mb-0">Projects</h2>
-        </div>
-    </div>
-
-    <div class="clearfix"></div>
-
-    
-   
     @foreach ($pr as $i)
         <div class="col-lg-4 col-md-6 col-12">
             <div class="projects-thumb">
